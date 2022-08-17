0210cee2fa8d36dfa6f9e65ea217719328b243bb
Sebastián Vicencio <sivicencio@gmail.com>
2022-08-17 14:21:10 -0400

    feat(task selector): add app structure and basic task selector
    * feat(app): add fastapi base endpoint with package requirements
    
    * feat(task selector): add basic structure for selecting tasks with validation
    
    * feat(task selector): add selector logic (first iteration)

---
+++ task_selector/handler.py
@@ -0,0 +1,22 @@
+from typing import List, Dict
+from pydantic import parse_obj_as, ValidationError
+from .models import Task
+from .selector import select_tasks
+
+class Handler:
+    def __init__(self, tasks: List[Task] = []):
+        self.tasks = self.build_and_validate(tasks)
+
+    def build_and_validate(self, tasks: List[Task]) -> List[Task]:
+        try:
+            return parse_obj_as(List[Task], tasks)
+        except ValidationError as e:
+            print(e.errors())
+            return []
+
+    def perform_selection(self) -> Dict:
+        selected, not_selected = select_tasks(self.tasks)
+        return {
+            'selected': selected,
+            'not_selected': not_selected
+        }
