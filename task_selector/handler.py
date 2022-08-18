71500a96d6804c0758efee238844683f7460af00
Sebastián Vicencio <sivicencio@gmail.com>
2022-08-17 20:49:11 -0400

    feat(task selector): update handler to include standby tasks at selection

--- task_selector/handler.py
+++ task_selector/handler.py
@@ -2,10 +2,12 @@ from typing import List, Dict
 from pydantic import parse_obj_as, ValidationError
 from .models import Task
 from .selector import select_tasks
+from .storage import TaskStorage
 
 class Handler:
     def __init__(self, tasks: List[Task] = []):
         self.tasks = self.build_and_validate(tasks)
+        self.storage = TaskStorage()
 
     def build_and_validate(self, tasks: List[Task]) -> List[Task]:
         try:
@@ -15,7 +17,19 @@ class Handler:
             return []
 
     def perform_selection(self) -> Dict[str, List[Task]]:
-        selected, not_selected = select_tasks(self.tasks)
+        standby_tasks = self.storage.get_standby_tasks()
+
+        # Perform selection between standby tasks and received tasks
+        joined_tasks = [*standby_tasks, *self.tasks]
+        selected, not_selected = select_tasks(joined_tasks)
+
+        # Clear standby tasks since selection was successful
+        self.storage.clear_standby_tasks()
+
+        # Persist new standby (not selected) tasks
+        if len(not_selected) > 0:
+            self.storage.add_standby_tasks(not_selected)
+
         return {
             'selected': selected,
             'not_selected': not_selected
