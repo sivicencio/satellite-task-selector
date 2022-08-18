08ca7c0b4491bacf4681294da73f10b669849b04
Sebastián Vicencio <sivicencio@gmail.com>
2022-08-17 20:45:37 -0400

    feat(task selector): add task selector storage module with redis integration
    The storage module includes methods for handling standby tasks as a redis list
    Methods allow to get, add and clear standby tasks

---
+++ task_selector/storage.py
@@ -0,0 +1,38 @@
+import redis
+from typing import List
+from .models import Task
+
+KEYS = {
+    'standby': 'tasks:standby'
+}
+
+MAX_ITEMS = {
+    'standby': 100
+}
+
+class TaskStorage:
+    def __init__(self):
+        self.storage = redis.Redis(decode_responses=True)
+    
+    def get_standby_tasks(self) -> List[Task]:
+        return self.__get_tasks(KEYS['standby'])
+
+    def add_standby_tasks(self, tasks: List[Task]) -> int:
+        return self.__add_tasks(tasks, KEYS['standby'], MAX_ITEMS['standby'])
+
+    def clear_standby_tasks(self) -> bool:
+        return self.__clear_tasks(KEYS['standby'])
+    
+    def __get_tasks(self, key: str) -> List[Task]:
+        tasks_in_json = self.storage.lrange(key, 0, -1)
+        return list(map(lambda task_json: Task.parse_raw(task_json), tasks_in_json))
+
+    def __add_tasks(self, tasks: List[Task], key: str, max_items: int) -> int:
+        for task in tasks:
+            self.storage.lpush(key, task.json())
+        self.storage.ltrim(key, 0, max_items - 1)
+        return len(tasks)
+
+    def __clear_tasks(self, key: str) -> bool:
+        deleted_keys_count = self.storage.delete(key)
+        return True if deleted_keys_count in [0, 1] else False
