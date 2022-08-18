db57448400ca3bc0dd3675261d9e6a218ab2e0d0
Sebastián Vicencio <sivicencio@gmail.com>
2022-08-17 21:49:29 -0400

    feat(task selector): add tasks persistence and handling endpoints (#11)
    * feat(task selector): add task selector storage module with redis integration
    
    * feat(task selector): update handler to include standby tasks at selection
    
    * feat(task selector): update handler to persist selected tasks
    
    * feat(tasks): add endpoints for retrieving selected and standby tasks
    
    * feat(tasks): add endpoints for clearing selected and standby tasks

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1,12 +1,33 @@
-from fastapi import APIRouter
-from typing import List
+from fastapi import APIRouter, status
+from typing import Dict, List
 from task_selector.handler import Handler
 from task_selector.models import Task
+from task_selector.storage import TaskStorage
 
 router = APIRouter()
 
-@router.post("/", tags=['tasks'])
+@router.post("/", tags=['tasks'], response_model=Dict[str, List[Task]])
 async def select_tasks(tasks: List[Task]):
     selector_handler = Handler(tasks)
     selection_result = selector_handler.perform_selection()
     return selection_result
+
+@router.get("/selected", tags=['tasks'], response_model=List[Task])
+async def get_selected_tasks():
+    storage = TaskStorage()
+    return storage.get_selected_tasks()
+
+@router.get("/standby", tags=['tasks'], response_model=List[Task])
+async def get_standby_tasks():
+    storage = TaskStorage()
+    return storage.get_standby_tasks()
+
+@router.delete("/selected", tags=['tasks'], status_code=status.HTTP_204_NO_CONTENT)
+async def clear_selected_tasks():
+    storage = TaskStorage()
+    storage.clear_selected_tasks()
+
+@router.delete("/standby", tags=['tasks'], status_code=status.HTTP_204_NO_CONTENT)
+async def clear_standby_tasks():
+    storage = TaskStorage()
+    storage.clear_standby_tasks()
---
+++ task_selector/storage.py
@@ -0,0 +1,49 @@
+import redis
+from typing import List
+from .models import Task
+
+KEYS = {
+    'selected': 'tasks:selected',
+    'standby': 'tasks:standby'
+}
+
+MAX_ITEMS = {
+    'selected': 100,
+    'standby': 100
+}
+
+class TaskStorage:
+    def __init__(self):
+        self.storage = redis.Redis(decode_responses=True)
+    
+    def get_standby_tasks(self) -> List[Task]:
+        return self.__get_tasks(KEYS['standby'])
+
+    def add_standby_tasks(self, tasks: List[Task]) -> int:
+        return self.__add_tasks(tasks, KEYS['standby'], MAX_ITEMS['standby'])
+
+    def clear_standby_tasks(self) -> bool:
+        return self.__clear_tasks(KEYS['standby'])
+
+    def get_selected_tasks(self) -> List[Task]:
+        return self.__get_tasks(KEYS['selected'])
+
+    def add_selected_tasks(self, tasks: List[Task]) -> int:
+        return self.__add_tasks(tasks, KEYS['selected'], MAX_ITEMS['selected'])
+
+    def clear_selected_tasks(self) -> bool:
+        return self.__clear_tasks(KEYS['selected'])
+
+    def __get_tasks(self, key: str) -> List[Task]:
+        tasks_in_json = self.storage.lrange(key, 0, -1)
+        return list(map(lambda task_json: Task.parse_raw(task_json), tasks_in_json))
+
+    def __add_tasks(self, tasks: List[Task], key: str, max_items: int) -> int:
+        for task in tasks:
+            self.storage.lpush(key, task.json())
+        self.storage.ltrim(key, 0, max_items - 1)
+        return len(tasks)
+
+    def __clear_tasks(self, key: str) -> bool:
+        deleted_keys_count = self.storage.delete(key)
+        return True if deleted_keys_count in [0, 1] else False
